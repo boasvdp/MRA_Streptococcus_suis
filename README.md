de9c6db20afb75e3a738eee48bc885ecd7aafe2a
boasvdp <boasvdp@gmail.com>
2019-10-16 11:08:17 +0200

    Added download script, updated readme and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# MRA_Streptoccus_suis
+# MRA_Streptococcus_suis
 Repository for the Microbiology Resource Announcements paper on several closed Streptococcus suis genomes
+
+## Introduction
+
+## Methods
+
+## Results
---
+++ download_ENA.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/boas/experiments/MRA_Streptococcus_suis
+awk 'NR > 1 {print $2}' accessions.txt > list
+$HOME/tools/getSeqENA/getSeqENA.py -l list -o raw_illumina/
+
+awk 'NR>1 {print $0}' accessions.txt | while read sample acc
+do
+	mv raw_illumina/${acc}/${acc}_1.fq.gz raw_illumina/${sample}_1.fastq.gz
+	mv raw_illumina/${acc}/${acc}_2.fq.gz raw_illumina/${sample}_2.fastq.gz
+	rm -r raw_illumina/${acc}
+done
+
+rm raw_illumina/getSeqENA.report.txt raw_illumina/run*log list
